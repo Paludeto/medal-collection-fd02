baf829da997c69ad206b021edb720329b074abe7
Paludeto <gpalu54@gmail.com>
2024-05-22 18:16:12 -0300

    parser completo usando regex

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ int main() {
     Medalha medalhas[NUM_MEDALHAS];
 
     FILE *arq;
-    arq = fopen("../data/medalhas.csv", "r");
+    arq = fopen("../../data/medalhas.csv", "r");
     int contador = 0;
 
     if(arq == NULL) {
@@ -16,18 +16,25 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    while (!feof(arq)) {
-        int campos = fscanf(arq, "%c,%63[^,],%63[^,],%d,%c,%63[^,],%63[^,],%63[^,]\n",
-                    &medalhas[contador].genero, &medalhas[contador].modalidade, &medalhas[contador].cidade, &medalhas[contador].ano, &medalhas[contador].gbs,
-                    &medalhas[contador].nome, &medalhas[contador].pais, &medalhas[contador].resultado);
+    while (contador < NUM_MEDALHAS) {
+
+        int campos = fscanf(arq, "%c,%63[^,],%63[^,],%d,%c,%63[^,],%63[^,],%63[^\n]\n",
+                    &medalhas[contador].genero, medalhas[contador].modalidade, medalhas[contador].cidade, &medalhas[contador].ano, &medalhas[contador].gbs,
+                    medalhas[contador].nome, medalhas[contador].pais, medalhas[contador].resultado);
+
         if (campos != 8){
             break;
         }
+
+        contador++;
+
     }
 
-    for (int i = 0; i < NUM_MEDALHAS; i++) {
-        printf("%c, %s, %s, %d, %c, %s, %s, %s\n", medalhas[contador].genero, medalhas[contador].modalidade, medalhas[contador].cidade, medalhas[contador].ano, medalhas[contador].gbs,
-                    medalhas[contador].nome, medalhas[contador].pais, medalhas[contador].resultado);
+    for (int i = 0; i < contador; i++) {  
+
+        printf("%c, %s, %s, %d, %c, %s, %s, %s\n", medalhas[i].genero, medalhas[i].modalidade, medalhas[i].cidade, medalhas[i].ano, medalhas[i].gbs,
+                    medalhas[i].nome, medalhas[i].pais, medalhas[i].resultado);
+                    
     }
 
     fclose(arq);
