8157d1aafe5d3fd5acb0d3e34e91f44574d3349f
Paludeto <gpalu54@gmail.com>
2024-06-07 13:49:45 -0300

    funções de exibição e busca

--- src/medalha.c
+++ src/medalha.c
@@ -1,7 +1,8 @@
 #include "../include/medalha.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <ctype.h>
+#include <string.h>
 
 int linhasArquivo(FILE *arq) {
 
@@ -101,4 +102,63 @@ void leBinario(FILE *arq, Medalha* medalhas, int numLinhas) {
     
     return;
     
+}
+
+void exibeAtleta(Medalha **medalhas, int indice) {
+
+    printf("%-10s\t | %-10s\t | %-10s\t | %-10s\t | %-10s\t | %-10s\t | %-10s\t | %-10s\t |\n",
+        "GÊNERO", "MODALIDADE", "CIDADE", "ANO", "GBS", "NOME", "PAÍS", "RESULTADO");   
+    
+    printf("%-10s\t | %-10s\t | %-10s\t | %-10s\t | %-10s\t | %-10s\t | %-10s\t | %-10s\t |\n",
+        medalhas[indice]->genero, medalhas[indice]->modalidade, medalhas[indice]->cidade, medalhas[indice]->ano, 
+        medalhas[indice]->gbs, medalhas[indice]->nome, medalhas[indice]->pais, medalhas[indice]->resultado);  
+
+    return;
+
+}
+
+int buscaAtleta(Medalha **medalhas, int tamanhoArray, char nomeBuscado[TAM_STRING]) {
+
+    for (int i = 0; i < tamanhoArray; i++) {
+        if (strcmp(nomeBuscado, medalhas[i]->nome) == 0) {
+            return i;
+        }
+    }
+
+    return -1;
+
+}
+
+void modificaAtleta(Medalha** medalhas, int tamanhoArray) {
+
+    typedef enum OPCOES {
+        GENERO = 1,
+        MODALIDADE,
+        CIDADE, 
+        ANO,
+        GBS, 
+        NOME,
+        PAIS,
+        RESULTADO
+    };
+
+    char nomeBuscado[TAM_STRING];
+    fgets(nomeBuscado, TAM_STRING, stdin);
+    nomeBuscado[strcspn(nomeBuscado, "\n")] = '\0';
+
+    int indice = buscaAtleta(medalhas, tamanhoArray, nomeBuscado);
+
+    if (indice == -1) {
+        perror("Atleta não encontrado!\n");
+        return;
+    }
+
+    exibeAtleta(medalhas, indice);
+
+    printf("Qual campo do atleta você deseja modificar?\n");
+    printf("1 - GÊNERO\n2 - MODALIDADE\n3 - CIDADE\n4 - ANO\n 5 - GBS\n6 - NOME\n7 - PAIS\n8 - RESULTADO\n");
+    
+    //inacabada
+    return;
+
 }
